1ea221365479c82638013e1a300bf55975cda7fd
Leonardo0294 <chichelezcano94@hotmail.com.ar>
2024-05-05 17:32:05 -0300

    se agregaron cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import lineChartImage from './assets/Grafico-linea.png';
 import barChartImage from './assets/Grafico-barra.png';
-import './styles.css'
+import plotyChartImage from './assets/GraficoPloty.png';
+import './styles.css';
+
 const openImageInNewTab = (imageUrl) => {
     const image = new Image();
     image.src = imageUrl;
 
-    const width = image.width;
-    const height = image.height;
+    image.onload = () => {
+        const width = image.width;
+        const height = image.height;
 
-    // Calcula el tamaño de la ventana emergente basado en las dimensiones de la imagen
-    const windowWidth = width + 20; // Se agrega un poco de espacio adicional
-    const windowHeight = height + 20; // Se agrega un poco de espacio adicional
+        // Calcula el tamaño de la ventana emergente basado en las dimensiones de la imagen
+        const windowWidth = width + 20; // Se agrega un poco de espacio adicional
+        const windowHeight = height + 20; // Se agrega un poco de espacio adicional
 
-    // Abre una nueva ventana con la imagen en tamaño completo
-    window.open(imageUrl, '_blank', `width=${windowWidth}, height=${windowHeight}`);
+        // Abre una nueva ventana con la imagen en tamaño completo
+        window.open(imageUrl, '_blank', `width=${windowWidth}, height=${windowHeight}`);
+    };
 };
 
 const App = () => {
@@ -26,18 +30,22 @@ const App = () => {
         openImageInNewTab(barChartImage);
     };
 
+    const showPlotyChart = () => {
+        openImageInNewTab(plotyChartImage);
+    };
+
     return (
         <div style={{ textAlign: 'center' }}>
             <h1>Billboard Hot 100 Charts</h1>
             <p>
-                A continuación le presentamos dos gráficos hechos con Google Charts, basados en la lista "Billboard Hot 100
-                Songs" desde 1958 hasta 2021.
+                A continuación le presentamos dos gráficos hechos con Google Charts, basados en la lista "Billboard Hot 100 Songs" desde 1958 hasta 2021.
             </p>
             <div style={{ marginBottom: '20px' }}>
                 <button onClick={showLineChart}>Mostrar Gráfico de Líneas</button>
                 <button onClick={showBarChart}>Mostrar Gráfico de Barras</button>
+                {/* Agregar un botón para mostrar la imagen de PlotyChart */}
+                <button onClick={showPlotyChart}>Mostrar Gráfico de Ploty</button>
             </div>
-            {/* Elimina las imágenes de aquí ya que se abrirán en una nueva ventana */}
         </div>
     );
 };
