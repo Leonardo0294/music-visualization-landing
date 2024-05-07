ad37f76aec414f04636227fa1fabdea4b719f3ed
Leonardo0294 <chichelezcano94@hotmail.com.ar>
2024-05-07 12:38:54 -0300

    feat:se agrego otro grafico de promedio de canciones por artistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import lineChartImage from './assets/Grafico-linea.png';
 import barChartImage from './assets/Grafico-barra.png';
-import plotyChartImage from './assets/GraficoPloty.png';
+import plotlyChartImage from './assets/GraficoPloty.png';
 import tortaChartImage from './assets/Grafico-Torta.png';
+import promedioChartImage from './assets/promedio-artista.png';
 import './styles.css';
 
 const openImageInNewTab = (imageUrl) => {
@@ -13,12 +14,14 @@ const openImageInNewTab = (imageUrl) => {
         const width = image.width;
         const height = image.height;
 
-        // Calcula el tamaño de la ventana emergente basado en las dimensiones de la imagen
-        const windowWidth = width + 20; // Se agrega un poco de espacio adicional
-        const windowHeight = height + 20; // Se agrega un poco de espacio adicional
+        const windowWidth = width + 20;
+        const windowHeight = height + 20;
 
-        // Abre una nueva ventana con la imagen en tamaño completo
         window.open(imageUrl, '_blank', `width=${windowWidth},height=${windowHeight}`);
+    };
+
+    image.onerror = (error) => {
+        console.error('Error al cargar la imagen:', error);
     };
 };
 
@@ -31,28 +34,34 @@ const App = () => {
         openImageInNewTab(barChartImage);
     };
 
-    const showPlotyChart = () => {
-        openImageInNewTab(plotyChartImage);
+    const showPlotlyChart = () => {
+        openImageInNewTab(plotlyChartImage);
     };
 
     const showTortaChart = () => {
         openImageInNewTab(tortaChartImage);
     };
 
+    const showArtistPromedio = () => {
+        openImageInNewTab(promedioChartImage);
+    };
+
     return (
         <div style={{ textAlign: 'center' }}>
             <h1>Graficos de Billboard Hot 100 Charts</h1>
             <p>
-                A continuación le presentamos dos gráficos, basados en la lista "Billboard Hot 100 Songs" desde 1958 hasta 2021.
+                A continuación le presentamos gráficos basados en la lista "Billboard Hot 100 Songs" desde 1958 hasta 2021.
             </p>
             <div style={{ marginBottom: '20px' }}>
                 <button onClick={showLineChart}>Mostrar Gráfico de Líneas</button>
                 <button onClick={showBarChart}>Mostrar Gráfico de Barras</button>
-                <button onClick={showPlotyChart}>Mostrar Gráfico de Ploty</button>
+                <button onClick={showPlotlyChart}>Mostrar Gráfico de Plotly</button>
                 <button onClick={showTortaChart}>Mostrar Gráfico de Torta</button>
+                <button onClick={showArtistPromedio}>Mostrar Promedio por Artista</button>
             </div>
         </div>
     );
 };
 
 export default App;
+
